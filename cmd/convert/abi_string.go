027ec644e53d7d84fa097474add77e5d20539cfc
0xbcdev <0xbcdev@bcdev.tools>
2024-04-11 14:00:14 +0700

    `convert abi_string` now only print either result

--- cmd/convert/abi_string.go
+++ cmd/convert/abi_string.go
@@ -20,22 +20,32 @@ Support pipe.`,
 			utils.RequireArgs(args, cmd)
 
 			if len(args) == 1 && len(args[0]) >= 192 {
-				abiToString := func(input string) {
+				abiToString := func(input string) (nonEmpty bool, decodeErr error) {
 					bz, decodeErr := hex.DecodeString(input)
-					utils.ExitOnErr(decodeErr, "failed to decode hex string: "+input)
+					if decodeErr != nil {
+						return false, decodeErr
+					}
 					str, decodeErr := utils.AbiDecodeString(bz)
-					utils.ExitOnErr(decodeErr, "failed to decode ABI string: "+input)
-					fmt.Println("\n# ABI encoded hex string to string:")
+					if decodeErr != nil {
+						return false, decodeErr
+					}
+					if len(str) == 0 {
+						return false, nil
+					}
 					fmt.Println(str)
+					return true, nil
 				}
-				if regexp.MustCompile(`^0x[a-fA-F\d]+$`).MatchString(args[0]) && (len(args[0])-2 /*exclude 0x*/)%64 == 0 {
-					abiToString(args[0][2:])
-				} else if regexp.MustCompile(`^[a-fA-F\d]+$`).MatchString(args[0]) && (len(args[0]))%64 == 0 {
-					abiToString(args[0])
+				if regexp.MustCompile(`^0x[a-f\d]+$`).MatchString(args[0]) && (len(args[0])-2 /*exclude 0x*/)%64 == 0 {
+					if nonEmpty, err := abiToString(args[0][2:]); err == nil && nonEmpty {
+						return
+					}
+				} else if regexp.MustCompile(`^[a-f\d]+$`).MatchString(args[0]) && (len(args[0]))%64 == 0 {
+					if nonEmpty, err := abiToString(args[0]); err == nil && nonEmpty {
+						return
+					}
 				}
 			}
 
-			fmt.Println("\n# String to ABI encoded hex:")
 			bz, err := utils.AbiEncodeString(strings.Join(args, " "))
 			utils.ExitOnErr(err, "failed to encode string to ABI hex")
 			fmt.Println("0x" + hex.EncodeToString(bz))
