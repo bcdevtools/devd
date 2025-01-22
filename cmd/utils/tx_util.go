807af559dbf1708480d967f7ff3125e18f6d3af6
0xbcdev <0xbcdev@bcdev.tools>
2025-01-22 23:52:42 +0700

    add new feature to decode raw EVM tx

--- cmd/utils/tx_util.go
+++ cmd/utils/tx_util.go
@@ -1,6 +1,14 @@
 package utils
 
-import "regexp"
+import (
+	"encoding/hex"
+	"encoding/json"
+	"math/big"
+	"regexp"
+	"strings"
+
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+)
 
 type TxHashType uint8
 
@@ -18,4 +26,84 @@ func DetectTxHashType(txHash string) TxHashType {
 	} else {
 		return TxHashTypeInvalid
 	}
+}
+
+func DecodeRawEvmTx(rawTx string) (*ethtypes.Transaction, error) {
+	rawTx = strings.TrimPrefix(rawTx, "0x")
+	decoded, err := hex.DecodeString(rawTx)
+	if err != nil {
+		return nil, err
+	}
+
+	tx := &ethtypes.Transaction{}
+	err = tx.UnmarshalBinary(decoded)
+	if err != nil {
+		return nil, err
+	}
+
+	return tx, nil
+}
+
+type PrettyMarshalJsonEvmTxOption struct {
+	InjectFrom               bool
+	InjectHexTranslatedField bool
+}
+
+func MarshalPrettyJsonEvmTx(tx *ethtypes.Transaction, option *PrettyMarshalJsonEvmTxOption) ([]byte, error) {
+	bz, err := tx.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+
+	var _map map[string]interface{}
+	err = json.Unmarshal(bz, &_map)
+	if err != nil {
+		return nil, err
+	}
+
+	if option != nil {
+		if option.InjectHexTranslatedField {
+			tryInjectHexTranslatedField := func(name string) {
+				defer func() {
+					_ = recover() // omit any panic
+				}()
+
+				v, found := _map[name]
+				if !found || v == nil {
+					return
+				}
+
+				hexStr, ok := v.(string)
+				if !ok || !strings.HasPrefix(hexStr, "0x") || len(hexStr) < 3 {
+					return
+				}
+				hexStr = hexStr[2:]
+
+				bi, ok := new(big.Int).SetString(hexStr, 16)
+				if !ok {
+					return
+				}
+
+				_map["_"+name] = bi.String()
+			}
+			tryInjectHexTranslatedField("chainId")
+			tryInjectHexTranslatedField("gas")
+			tryInjectHexTranslatedField("gasPrice")
+			tryInjectHexTranslatedField("maxFeePerGas")
+			tryInjectHexTranslatedField("maxPriorityFeePerGas")
+			tryInjectHexTranslatedField("nonce")
+			tryInjectHexTranslatedField("value")
+		}
+
+		if option.InjectFrom {
+			signer := ethtypes.LatestSignerForChainID(tx.ChainId())
+			from, err := ethtypes.Sender(signer, tx)
+			if err != nil {
+				return nil, err
+			}
+			_map["_from"] = from.String()
+		}
+	}
+
+	return json.MarshalIndent(_map, "", "  ")
 }
